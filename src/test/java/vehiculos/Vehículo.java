0b496530b207040978074c568b832297b49ee78c
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-02 22:15:18 -0500

    Update Vehículo.java

--- src/test/java/vehiculos/Vehículo.java
+++ src/test/java/vehiculos/Vehículo.java
@@ -1,5 +1,111 @@
 package vehiculos;
 
 public class Vehículo {
+	
+	protected String placa;
+	protected int puertas;
+	protected double velocidadMaxima;
+	protected String nombre;
+	protected double precio;
+	protected double peso;
+	protected String traccion;
+	protected Fabricante fabricante;
+	protected static int CantidadVehiculos;
+	
+	public Vehículo (String placa, int puertas, double velocidadMaxima, String nombre, double precio, double peso, String traccion, Fabricante fabricante){
+		
+		this.placa = placa;
+		this.puertas= puertas;
+		this.nombre=nombre;
+		this.precio=precio;
+		this.peso=peso;
+		this.traccion=traccion;
+		this.fabricante=fabricante;
+		CantidadVehiculos++;
+		
+	}
+	
+	
+	
+	
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+
+	public double getVelocidadMaxima() {
+		return velocidadMaxima;
+	}
+
+	public void setVelocidadMaxima(int velocidadMaxima) {
+		this.velocidadMaxima = velocidadMaxima;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public String getTraccion() {
+		return traccion;
+	}
+
+	public void setTraccion(String traccion) {
+		this.traccion = traccion;
+	}
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public static int getCantidadVehiculos() {
+		return CantidadVehiculos;
+	}
+
+	public static void setCantidadVehiculos(int cantidadVehiculos) {
+		CantidadVehiculos = cantidadVehiculos;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
