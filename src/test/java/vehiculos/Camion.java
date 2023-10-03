97aaa63405cb14f0bb8f056e746a588eeed96eff
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-02 22:28:52 -0500

    Update Camion.java

--- src/test/java/vehiculos/Camion.java
+++ src/test/java/vehiculos/Camion.java
@@ -1,5 +1,31 @@
 package vehiculos;
 
 public class Camion extends Vehículo{
+	
+	private int ejes;
+	private static int camiones;
+	
+	
+	public Camion(String placa, String nombre, int precio, int peso,Fabricante fabricante,int ejes) {
+		super(placa,2,80, nombre, precio, peso, "4X2", fabricante);
+		this.ejes=ejes;
+		camiones++;
+	}
 
+	public int getEjes() {
+		return ejes;
+	}
+
+	public void setEjes(int ejes) {
+		this.ejes = ejes;
+	}
+
+	public static int getCamiones() {
+		return camiones;
+	}
+
+	
 }
+
+	
+	
