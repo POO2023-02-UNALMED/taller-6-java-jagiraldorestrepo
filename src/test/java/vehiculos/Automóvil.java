f2d0e47c2b7dd0da5853f7a64caa61567211867c
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-02 22:26:40 -0500

    3

--- src/test/java/vehiculos/Automóvil.java
+++ src/test/java/vehiculos/Automóvil.java
@@ -13,12 +13,21 @@ public class Automóvil extends Vehículo{
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
+	public int getPuertas() {
+		return puertas;
+	}
 
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+
+	public static int getAutomoviles() {
+		return automoviles;
+	}
 }
+	
+	
+	
+	
+	
+	
--- src/test/java/vehiculos/Camioneta.java
+++ src/test/java/vehiculos/Camioneta.java
@@ -1,5 +1,28 @@
 package vehiculos;
 
 public class Camioneta extends Vehículo {
+	private boolean volco;
+	private static int camionetas;
+	
+	public Camioneta(String placa, int puertas, String nombre, int precio, int peso,Fabricante fabricante,Boolean volco) {
+		super(placa, puertas,90, nombre, precio, peso, "4X4", fabricante);
+		this.volco=volco;
+	}
+	
+	public Boolean getVolco() {
+		return volco;
+	}
+
+	public void setVolco(Boolean volco) {
+		this.volco = volco;
+	}
+
+	public static int getCamionetas() {
+		return camionetas;
+	}
+	
+	
+	
+	
 
 }
