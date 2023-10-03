d9b1a6cd88f27fd7cfdc6f4b993d69d0683ea7fa
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-02 22:22:19 -0500

    Update Automóvil.java

--- src/test/java/vehiculos/Automóvil.java
+++ src/test/java/vehiculos/Automóvil.java
@@ -1,5 +1,24 @@
 package vehiculos;
 
 public class Automóvil extends Vehículo{
+	
+	
+	private int puestos;
+	private static int automoviles;
+	
+	public Automóvil(String placa, String nombre, double precio, double peso, Fabricante fabricante,int puertas) {
+		super(placa,4, 100, nombre, precio, peso,"FWD", fabricante);
+		this.puertas=puertas;
+		automoviles++;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
